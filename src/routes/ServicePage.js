7a42d3c1305d6b487d53ff6579638a6ec3b6f06a
肖智宸 <zhichen@xiaozhichendeMacBook-Pro.local>
2017-06-28 10:44:22 +0800

    add page title

--- src/routes/ServicePage.js
+++ src/routes/ServicePage.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {Layout} from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
 import ServiceComponent from './../components/ServiceComponent';
+import DocumentTitle from 'react-document-title';
+import autoAdaptive from './../utils/autoAdaptive';
+
+autoAdaptive();
 
 function ServicePage() {
     return (
         <div>
+            <DocumentTitle title='保障服务'></DocumentTitle>
             <ServiceComponent />
         </div>
     );
